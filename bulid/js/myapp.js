3c7542d4a815b8805edd5f78cfc69f5a727f24d9
Jay <359390851@qq.com>
2016-12-07 20:36:32 +0800

    bug小调整

--- bulid/js/myapp.js
+++ bulid/js/myapp.js
@@ -36,3 +36,21 @@ home.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $
         templateUrl: "tpls/css3(2).html"
     });
 }]);
+home.controller("topCtrl",["$scope",function($scope){
+	$(function () {
+    $(".toTop").hide();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            $(".toTop").fadeIn("slow");
+        } else {
+            $(".toTop").fadeOut("slow");
+        }
+    });
+    $(".toTop").click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+}]);
