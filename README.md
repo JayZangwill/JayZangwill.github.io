dfe3d9dc1c8d2b00b500fd9fcbc4c9a172488417
Jay <359390851@qq.com>
2017-02-27 21:28:15 +0800

    简历修改

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
      //运行 npm run dev
      //打包 npm run build
     
-1. 个人主页的第四次大更新，用vue-cli搭建的多页面主页，支持scss，es6。:tada:
+1. 个人主页的第四次大更新，用vue-cli搭建的多页面主页，支持scss，es6。主页目前正常运行与腾讯云上，系统是CentOS 64位:tada:
 
-3. 主页不支持ie9以下浏览器。
+3. 主页不支持ie9以下浏览器,不要问为什么！
 
 4. 涉及技术：javscript、bootstrap、vue、webpack等。
 
@@ -21,6 +21,6 @@
 
 2. 引入了bootstrap和grunt
 
-3. 引入了angular将grunt改为glup
+3. 引入了angular将grunt改为glup，并且主页搬迁到了腾讯云上
 
 4. 将angular和gulp改为vue和webpack
--- src/asssets/views/resume/skills.vue
+++ src/asssets/views/resume/skills.vue
@@ -10,12 +10,13 @@
 			<div class="col-md-12 describe">
 				<h3>补充</h3>
 				<ol>
-					<li>熟悉web语义化及w3c标准，熟练掌握DOM、BOM、AJAX、JSON。</li>
+					<li>熟悉web语义化及w3c标准，熟练掌握DOM、BOM、AJAX、JSON。热衷研究原生js。</li>
 					<li>曾阅读过jQuery的部分源码，能自己<strong>独立封装出</strong>jQuery的一些常用功能</li>
 					<li>注重代码的规范性，和浏览器及手机端的兼容</li>
 					<li>能够熟练使用git&github进行团队合作</li>
 					<li>比较注重代码的<strong>整洁和复用性</strong>，追求<strong>高性能且简洁的代码</strong></li>
-					<li>了解node和mongodb并且独立设计出一套基于node和mongodb的系统</li>
+					<li>了解node和mongodb并且独立设计出一套基于node和mongodb的<a href="http://www.jayzangwill.cn/bank-card/index.html">系统</a></li>
+					<li>有linux系统使用经验</li>
 					<li>关注新技术，熟悉es6，并且有一颗勇于探索未知的心</li>
 					<li>追求完美，代码出现bug会想各种办法解决</li>
 					<li>有较强的学习能力做事认真，性格开朗</li>
