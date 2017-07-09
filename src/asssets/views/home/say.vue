d88c7d320655a09b52da5e54d89ba50ce1833a44
Jay Zangwill <359390851@qq.com>
2017-07-09 10:56:57 +0800

    修改评论框

--- src/asssets/views/home/say.vue
+++ src/asssets/views/home/say.vue
@@ -1,32 +1,52 @@
 <template>
 	<div class="container">
-		<h3 @click="toggleDuoshuoComments('#comment-box');">看不到评论框的话点我</h3>
-		<hr/>
 		<div class="row">
 			<div class="col-md-12">
-				<div id="comment-box"></div>
-				<!-- 多说评论框 start -->
-				<div class="ds-thread" data-thread-key="jayz" data-title="留言板" data-url="http://jayz.coding.io/say.html"></div>
+				<div id="SOHUCS"></div>
 			</div>
 		</div>
 	</div>
 </template>
-<style scoped>
-	h3{
-		cursor: pointer;
-	}
-</style>
 <script>
 	export default {
 		name: 'say',
-		methods: {
-			toggleDuoshuoComments: function(container) {
-				var el = document.createElement('div'); //该div不需要设置class="ds-thread"
-				el.setAttribute('data-thread-key', 'jayz'); //必选参数
-				el.setAttribute('data-url', 'http://jayz.coding.io/say.html'); //必选参数
-				DUOSHUO.EmbedThread(el);
-				$(container).append(el);
-			}
+		mounted() {
+			(function() {
+				var appid = 'cyt5L9IVR';
+				var conf = 'prod_080a7a63a104f5558e75294c4df27430';
+				var width = window.innerWidth || document.documentElement.clientWidth;
+				if(width < 960) {
+					window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
+				} else {
+					var loadJs = function(d, a) {
+						var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
+						var b = document.createElement("script");
+						b.setAttribute("type", "text/javascript");
+						b.setAttribute("charset", "UTF-8");
+						b.setAttribute("src", d);
+						if(typeof a === "function") {
+							if(window.attachEvent) {
+								b.onreadystatechange = function() {
+									var e = b.readyState;
+									if(e === "loaded" || e === "complete") {
+										b.onreadystatechange = null;
+										a()
+									}
+								}
+							} else {
+								b.onload = a
+							}
+						}
+						c.appendChild(b)
+					};
+					loadJs("https://changyan.sohu.com/upload/changyan.js", function() {
+						window.changyan.api.config({
+							appid: appid,
+							conf: conf
+						})
+					});
+				}
+			})();
 		}
 	}
 </script>
