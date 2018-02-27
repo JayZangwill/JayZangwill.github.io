2d117ebb5f882b2261c4a6f91d887e1af264dc9e
Jay Zangwill <359390851@qq.com>
2018-02-27 23:38:23 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 window.onload = function () {
     new Swiper('.swiper-container', {
-      spaceBetween: 30
+        spaceBetween: 30
     });
-  
+	let email = jay.$('#email'),
+	tip = jay.$('#tip'),
+	body = jay.$('body')
+	setTimeout(()=>{
+		tip.style.opacity = 0;
+		body.style.paddingTop = 0;
+	},10000)
+	email.addEventListener('click', function(e) {
+		e.preventDefault();
+	})
 }
