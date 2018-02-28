8873bbe754c231317307a73f04183893398730bd
Jay Zangwill <359390851@qq.com>
2018-02-28 10:20:48 +0800

    copy

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,21 @@
-window.onload = function () {
-    new Swiper('.swiper-container', {
-        spaceBetween: 30
-    });
-	let email = jay.$('#email'),
-	tip = jay.$('#tip'),
-	body = jay.$('body')
-	setTimeout(()=>{
-		tip.style.opacity = 0;
-		body.style.paddingTop = 0;
-	},10000)
-	email.addEventListener('click', function(e) {
-		e.preventDefault();
-	})
+window.onload = function() {
+  new Swiper('.swiper-container', {
+    spaceBetween: 30
+  });
+  let email = jay.$('#email'),
+    tip = jay.$('#tip'),
+    body = jay.$('body')
+  setTimeout(() => {
+    tip.style.opacity = 0;
+    body.style.paddingTop = 0;
+  }, 10000)
+  email.addEventListener('click', function(e) {
+    e.preventDefault();
+    let selection = getSelection(),
+      range = document.createRange();
+    selection.rangeCount > 0 && selection.removeAllRanges();
+    range.selectNode(this);
+    selection.addRange(range);
+    document.execCommand('copy');
+  })
 }
