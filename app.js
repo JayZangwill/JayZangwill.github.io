9591f8a18bd3d1a9b568d47545d6d73064d67874
Jay Zangwill <359390851@qq.com>
2018-10-08 14:21:06 +0800

    add resume download

--- app.js
+++ app.js
@@ -1,310 +1,77 @@
-const express = require ('express'),
-  router = express.Router (),
-  assert = require ('assert'),
-  fs = require ('fs'),
-  https = require ('https'),
-  querystring = require ('querystring'),
-  MongoClient = require ('mongodb').MongoClient,
-  url = 'mongodb://localhost:27017/myproject', // 数据库所在位置
-  compression = require ('compression'),
-  privateKey = fs.readFileSync ('./certificate/www.jayzangwill.cn.key', 'utf8'),
-  certificate = fs.readFileSync (
+const express = require('express'),
+  fs = require('fs'),
+  https = require('https'),
+  compression = require('compression'),
+  privateKey = fs.readFileSync('./certificate/www.jayzangwill.cn.key', 'utf8'),
+  certificate = fs.readFileSync(
     './certificate/www.jayzangwill.cn.crt',
     'utf8'
-  );
-app = express ();
-let userinfo; // 用户的信息
+  ),
+  app = express(),
+  cheerio = require('cheerio'),
+  superagent = require('superagent'),
+  card = require('./bank-card/server');
 
-app.all ('/', function (req, res, next) {
-  !req.secure
-    ? res.redirect (301, 'https://www.jayzangwill.cn' + req.url)
-    : next ();
+app.all('*', function (req, res, next) {
+  !req.secure && /(\.html)|(#.*)|\/$/.test(req.originalUrl) ?
+    res.redirect(301, 'https://www.jayzangwill.cn' + req.url) :
+    next();
 });
 
-app.use (compression ());
-app.use (express.static (__dirname));
+app.use(compression());
+app.use(express.static(__dirname));
 
-// 注册
-app.post ('/reg', (req, res) => {
-  let alldata = '';
-  req.on ('data', function (chunk) {
-    alldata += chunk;
-  });
-  req.on ('end', () => {
-    let dataString = alldata.toString (), // 将字符串转换为一个对象
-      dataObj = querystring.parse (dataString); // 将接收到的字符串转换位为json对象
-    MongoClient.connect (url, (err, db) => {
-      assert.equal (null, err);
-      console.log (getTime ());
-      console.log ('connect completed!\n===============\n');
-      findDocuments (db, doc => {
-        for (let i = 0; i < doc.length; i++) {
-          if (doc[i].idCard === dataObj.idCard) {
-            db.close ();
-            res.json ({
-              message: 'repeat',
-            });
-            return;
-          }
-        }
-        insertDocuments (db, dataObj, () => {
-          db.close ();
-          userinfo = dataObj.idCard;
-          res.json ({
-            message: 'success',
-          });
-          console.log (getTime ());
-          console.log ('register completed!\n===============\n');
-        });
+app.get('/article', (req, res, next) => {
+  superagent.get('https://jayzangwill.github.io/blog/').end((err, main) => {
+    if (err) {
+      console.error(err);
+      res.json({
+        message: 'err',
       });
-    });
-  });
-});
-
-// 登陆
-app.post ('/log', (req, res) => {
-  let alldata = '';
-  req.on ('data', function (chunk) {
-    alldata += chunk;
-  });
-  req.on ('end', () => {
-    let dataString = alldata.toString (),
-      dataObj = querystring.parse (dataString);
-    MongoClient.connect (url, (err, db) => {
-      assert.equal (null, err);
-      console.log (getTime ());
-      console.log ('connect completed!\n===============\n');
-      findDocuments (
-        db,
-        doc => {
-          for (let i = 0; i < doc.length; i++) {
-            if (doc[i].idCard === dataObj.idCard) {
-              // 如果用户输入身份证在数据库中存在
-              if (doc[i].password === dataObj.password) {
-                // 如果密码正确则登陆成功
-                db.close ();
-                res.json ({
-                  message: 'success',
-                });
-                userinfo = dataObj.idCard;
-                console.log (getTime ());
-                console.log ('login success!\n===============\n');
-                return;
-              } else {
-                // 密码错误的话返回错误信息
-                db.close ();
-                res.json ({
-                  message: 'password error',
-                });
-                console.log (getTime ());
-                console.log ('password error\n===============\n');
-                return;
-              }
-            }
-          }
-
-          // 否则就是输入的身份证不存在
-          db.close ();
-          res.json ({
-            message: 'nofound',
-          });
-          console.log (getTime ());
-          console.log ('idCard nofound\n===============\n');
-        },
-        {
-          idCard: dataObj.idCard,
-        }
-      );
-    });
-  });
-});
-
-// 存款
-app.post ('/deposit', (req, res) => {
-  let alldata = '';
-  req.on ('data', function (chunk) {
-    alldata += chunk;
-  });
-  req.on ('end', () => {
-    // 如果用户未登录，让用户去登录
-    if (!userinfo) {
-      res.json ({
-        message: 'nologin',
-      });
-      return;
+      next();
     }
-    let dataString = alldata.toString (),
-      dataObj = querystring.parse (dataString);
-    MongoClient.connect (url, (err, db) => {
-      assert.equal (null, err);
-      console.log (getTime ());
-      console.log ('connect completed!\n===============\n');
-      findDocuments (
-        db,
-        doc => {
-          let totalMoney = +doc[0].money + +dataObj.money;
-          updateDocument (db, userinfo, totalMoney, () => {
-            db.close ();
-            res.json ({
-              message: +dataObj.money,
-            });
-            console.log (getTime ());
-            console.log ('Deposit completed!\n===============\n');
-          });
-        },
-        {
-          idCard: userinfo,
-        }
+    let $ = cheerio.load(main.text);
+    let result = {
+      href: [],
+      title: [],
+      time: [],
+      description: [],
+    };
+    $('.article-title').each((_, element) => {
+      let $element = $(element);
+      result.href.push(
+        'https://jayzangwill.github.io' + $element.attr('href')
       );
+      result.title.push($element.text());
+    });
+    $('time').each((_, element) => {
+      let $element = $(element);
+      result.time.push($element.text());
+    });
+    $('.article-entry').each((_, element) => {
+      let $element = $(element);
+      result.description.push($element.text());
+    });
+    res.json({
+      message: 'success',
+      result,
     });
   });
 });
 
-// 取款
-app.post ('/draw', (req, res) => {
-  let alldata = '';
-  req.on ('data', function (chunk) {
-    alldata += chunk;
-  });
-  req.on ('end', function () {
-    if (!userinfo) {
-      res.json ({
-        message: 'nologin',
-      });
-      return;
-    }
-    let dataString = alldata.toString (),
-      dataObj = querystring.parse (dataString);
-    MongoClient.connect (url, (err, db) => {
-      assert.equal (null, err);
-      console.log (getTime ());
-      console.log ('connect completed!\n===============\n');
-      findDocuments (
-        db,
-        function (doc) {
-          let totalMoney = +doc[0].money - +dataObj.money;
-          if (totalMoney < 0) {
-            db.close ();
-            res.json ({
-              message: 'more',
-            });
-            return;
-          }
-          updateDocument (db, userinfo, totalMoney, () => {
-            db.close ();
-            res.json ({
-              message: +dataObj.money,
-            });
-            console.log ('withdrawal completed!\n===============\n');
-          });
-        },
-        {
-          idCard: userinfo,
-        }
-      );
-    });
-  });
-});
+card(app);
 
-// 查询余额
-app.get ('/findmoney', (req, res) => {
-  let alldata = '';
-  req.on ('data', function (chunk) {
-    alldata += chunk;
-  });
-  if (!userinfo) {
-    res.json ({
-      message: 'nologin',
-    });
-    return;
-  }
-  MongoClient.connect (url, (err, db) => {
-    assert.equal (null, err);
-    console.log (getTime ());
-    console.log ('connect completed!\n===============\n');
-    findDocuments (
-      db,
-      doc => {
-        db.close ();
-        res.json ({
-          message: +doc[0].money,
-        });
-        console.log (getTime ());
-        console.log ('found complete!\n===============\n');
-      },
-      {
-        idCard: userinfo,
-      }
-    );
-  });
-});
-
-// 登出
-app.get ('/out', () => {
-  userinfo = undefined;
+app.get('/download', function (req, res) {
+  res.download('./resume.pdf', 'resume.pdf', err => {
+    console.log(err)
+  })
 });
 
 // 创建服务器;
-app.listen (80);
-https
-  .createServer (
-    {
-      key: privateKey,
-      cert: certificate,
-    },
-    app
-  )
-  .listen (443, () => {
-    console.log ('server is runing!');
-  });
-
-//插入操作
-function insertDocuments (db, data, callback) {
-  let collection = db.db ('myproject').collection ('documents');
-  collection.insertOne (data, (err, result) => {
-    assert.equal (err, null);
-    console.log (getTime ());
-    console.log ('insert complete!\n===============\n=');
-    callback (result);
-  });
-}
-
-//查找操作
-function findDocuments (db, callback, search) {
-  // Get the documents collection
-  search = search || {};
-  let collection = db.db ('myproject').collection ('documents');
-  // Find some documents
-  collection.find (search).toArray ((err, docs) => {
-    assert.equal (err, null);
-    callback (docs);
-  });
-}
-
-//更新操作
-function updateDocument (db, userinfo, money, callback) {
-  // Get the documents collection
-  let collection = db.db ('myproject').collection ('documents');
-
-  // Update document where a is 2, set b equal to 1
-  collection.updateOne (
-    {
-      idCard: userinfo,
-    },
-    {
-      $set: {
-        money: money,
-      },
-    },
-    (err, result) => {
-      assert.equal (err, null);
-      callback (result);
-    }
-  );
-}
-
-function getTime () {
-  let d = new Date ();
-  let str = `${d.getFullYear ()}-${d.getMonth () + 1}-${d.getDate ()} ${d.getHours ()}:${d.getMinutes ()}:${d.getSeconds ()}`;
-  return str;
-}
+app.listen(80);
+https.createServer({
+  key: privateKey,
+  cert: certificate,
+}, app).listen(443, () => {
+  console.log('server is runing!');
+});
--- src/views/resume/introduct.vue
+++ src/views/resume/introduct.vue
@@ -1,61 +1,60 @@
 <template>
-	<div class="col-md-12 text-center">
-		<div class="info">
-			<div class="row">
-				<div class="col-md-12 text-center">
-					姓名：张杰
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6 text-center">
-					专业：网络工程 本科
-				</div>
-				<div class="col-md-6 text-center">
-					毕业时间：2018.7
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6 text-center">
-					手机：17839191579
-				</div>
-				<div class="col-md-6 text-center email">
-					邮箱：
-					<a href="mailto:jayzangwill@gmail.com">jayzangwill@gmail.com</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-					说明：因本人今年5-6月要回学校进行毕业设计和答辩，预计入职时间在6月中旬左右。
-				</div>
-			</div>
-		</div>
-		<div class="divi"></div>
-		<div class="introduct">
-			<h2 class="text-center">关于我</h2>
-			<p>在15年（也就是在我大二上学期的时候）一次偶然的机会加入了学校的信息技术实验室，在学长的“蛊惑”下跳入了前端的坑。并因此对前端产生了浓厚的兴趣（因为当时学了C语言，对着黑框框调代码实在太枯燥，入了前端坑仿佛发现了新大陆），在不断地学习，不断地卧槽，原来 HTML、CSS、JS 还可以这么玩，渐渐加深了自己对前端的热爱。
-			</p>
-			<p>对新技术抱有激情，渴望在技术上有所建树，希望自己做的东西能被大家喜欢。</p>
-			<p>另外，还有
-				<a href="https://github.com/JayZangwill">github</a>和
-				<a href="https://jayzangwill.github.io/blog/">博客</a>、
-				<a href="http://www.jayzangwill.cn/">主页</a>可供参观。</p>
-		</div>
-	</div>
+  <div class="col-md-12 text-center">
+    <div class="info">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          姓名：张杰
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 text-center">
+          专业：网络工程 本科
+        </div>
+        <div class="col-md-6 text-center">
+          毕业时间：2018.7
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 text-center">
+          手机：17839191579
+        </div>
+        <div class="col-md-6 text-center email">
+          邮箱：
+          <a href="mailto:jayzangwill@gmail.com">jayzangwill@gmail.com</a>
+        </div>
+      </div>
+    </div>
+    <div class="divi"></div>
+    <div class="introduct">
+      <h2 class="text-center">关于我</h2>
+      <p>在15年（也就是在我大二上学期的时候）一次偶然的机会加入了学校的信息技术实验室，在学长的“蛊惑”下跳入了前端的坑。并因此对前端产生了浓厚的兴趣（因为当时学了C语言，对着黑框框调代码实在太枯燥，入了前端坑仿佛发现了新大陆），在不断地学习，不断地卧槽，原来
+        HTML、CSS、JS 还可以这么玩，渐渐加深了自己对前端的热爱。
+      </p>
+      <p>对新技术抱有激情，渴望在技术上有所建树，希望自己做的东西能被大家喜欢。</p>
+      <p>另外，还有
+        <a href="https://github.com/JayZangwill">github</a>和
+        <a href="https://jayzangwill.github.io/blog/">博客</a>、
+        <a href="https://www.jayzangwill.cn/">主页</a>可供参观。
+      </p>
+      <p><a href="https://www.jayzangwill.cn/download">简历下载</a></p>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
-$white: #fff;
-$coffee: #b4531e;
+  $white: #fff;
+  $coffee: #b4531e;
 
-.photo {
+  .photo {
     overflow: hidden;
 
     margin: 20px 0;
-    img {
-        border-radius: 50%;
-    }
-}
 
-.info {
+    img {
+      border-radius: 50%;
+    }
+  }
+
+  .info {
     font-size: 20px;
 
     color: $white;
@@ -64,52 +63,60 @@ $coffee: #b4531e;
     -moz-user-select: text;
     -ms-user-select: text;
     user-select: text;
-}
+  }
 
-.row {
+  .row {
     margin: 10px 0;
-}
+  }
 
-.email {
+  .email {
     a {
-        &:link,
-        &:visited {
-            color: $coffee;
-        }
-        &:hover {
-            text-decoration: none;
-            color: lighten($coffee, 20%);
-        }
-    }
-}
 
-.introduct {
+      &:link,
+      &:visited {
+        color: $coffee;
+      }
+
+      &:hover {
+        text-decoration: none;
+        color: lighten($coffee, 20%);
+      }
+    }
+  }
+
+  .introduct {
     margin: 20px 0;
 
     font-size: 18px;
     text-align: left;
 
     color: $white;
-    p {
-        text-indent: 40px;
-        a {
-            &:link,
-            &:visited {
-                color: $coffee;
-            }
-            &:hover {
-                text-decoration: none;
-                color: lighten($coffee, 20%);
-            }
-        }
-        span {
-            color: $coffee;
-        }
-    }
-}
 
-.divi {
+    p {
+      text-indent: 40px;
+
+      a {
+
+        &:link,
+        &:visited {
+          color: $coffee;
+        }
+
+        &:hover {
+          text-decoration: none;
+          color: lighten($coffee, 20%);
+        }
+      }
+
+      span {
+        color: $coffee;
+      }
+    }
+  }
+
+  .divi {
     border: 1px solid $white;
     width: 100%;
-}
+  }
+
 </style>
