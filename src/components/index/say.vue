a607f5e8477262c38735ec6b0b98af0c205c7e62
Jay Zangwill <359390851@qq.com>
2018-10-12 11:04:46 +0800

    bug fix

--- src/components/index/say.vue
+++ src/components/index/say.vue
@@ -3,54 +3,45 @@
     <div id="SOHUCS"></div>
   </div>
 </template>
-
 <script>
   export default {
     name: 'say',
     mounted() {
       const appid = "cytASohTI";
       const conf = "prod_4a8ed26681d02bdc291288b15f5256de";
-      const width =
-        window.innerWidth || document.documentElement.clientWidth;
-      if (width < 960) {
-        window.document.write(
-          '<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="//changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' +
-          appid + '&conf=' + conf + '"><\/script>');
-      } else {
-        const loadJs = function (d, a) {
-          const c =
-            document.getElementsByTagName("head")[0] ||
-            document.head ||
-            document.documentElement;
-          const b = document.createElement("script");
-          b.setAttribute("type", "text/javascript");
-          b.setAttribute("charset", "UTF-8");
-          b.setAttribute("src", d);
-          if (typeof a === "function") {
-            if (window.attachEvent) {
-              b.onreadystatechange = function () {
-                const e = b.readyState;
-                if (e === "loaded" || e === "complete") {
-                  b.onreadystatechange = null;
-                  a();
-                }
-              };
-            } else {
-              b.onload = a;
-            }
+      const loadJs = function (d, a) {
+        const c =
+          document.getElementsByTagName("head")[0] ||
+          document.head ||
+          document.documentElement;
+        const b = document.createElement("script");
+        b.setAttribute("type", "text/javascript");
+        b.setAttribute("charset", "UTF-8");
+        b.setAttribute("src", d);
+        if (typeof a === "function") {
+          if (window.attachEvent) {
+            b.onreadystatechange = function () {
+              const e = b.readyState;
+              if (e === "loaded" || e === "complete") {
+                b.onreadystatechange = null;
+                a();
+              }
+            };
+          } else {
+            b.onload = a;
           }
-          c.appendChild(b);
-        };
-        loadJs(
-          "//changyan.sohu.com/upload/changyan.js",
-          function () {
-            window.changyan.api.config({
-              appid: appid,
-              conf: conf
-            });
-          }
-        );
-      }
+        }
+        c.appendChild(b);
+      };
+      loadJs(
+        "//changyan.sohu.com/upload/changyan.js",
+        function () {
+          window.changyan.api.config({
+            appid,
+            conf
+          });
+        }
+      );
     }
   }
 
